5d4149a0e0e3ee0ee38bdf1adba0b58c3559d374
Peter <bklynpeter@gmail.com>
2024-08-03 12:04:40 -0400

    update models and config db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-12 p-24">
-      <h1>Hello World</h1>
+      <h1 className="text-2xl font-semibold">Hello World</h1>
     </main>
   );
 }
