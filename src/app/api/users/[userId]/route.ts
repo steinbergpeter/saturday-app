d68e816c663c34b6189bcfd4d8d0b959606f6d4b
Peter <bklynpeter@gmail.com>
2024-08-03 20:40:32 -0400

    fix CORS

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -2,6 +2,12 @@ import prisma from "@/lib/db";
 import { userWithPostsSchema } from "@/lib/validators";
 import { NextRequest, NextResponse } from "next/server";
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 async function GET(req: NextRequest) {
   try {
     const userId = req.nextUrl.pathname.split("/").pop();
@@ -11,10 +17,10 @@ async function GET(req: NextRequest) {
       include: { posts: true },
     });
     const user = userWithPostsSchema.parse(rawData);
-    return NextResponse.json(user, { status: 200 });
+    return NextResponse.json(user, { status: 200, headers: corsHeaders });
   } catch (error) {
     console.error(error);
-    return NextResponse.json(error, { status: 400 });
+    return NextResponse.json(error, { status: 400, headers: corsHeaders });
   }
 }
 
@@ -24,10 +30,13 @@ async function DELETE(req: NextRequest) {
     if (!userId) throw new Error("User ID is required");
     console.log("/api/users/[userId]/route.ts/DELETE called id: ", userId);
     const deletedUser = await prisma.user.delete({ where: { id: userId } });
-    return NextResponse.json(deletedUser, { status: 200 });
+    return NextResponse.json(deletedUser, {
+      status: 200,
+      headers: corsHeaders,
+    });
   } catch (error) {
     console.error(error);
-    return NextResponse.json(error, { status: 400 });
+    return NextResponse.json(error, { status: 400, headers: corsHeaders });
   }
 }
 
